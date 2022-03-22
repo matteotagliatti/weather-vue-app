2c8a19fd77855756845daca21f3a00ebb255de56
Matteo Tagliatti <matteotagliatti@gmail.com>
2022-03-22 15:20:26 +0100

    first commit. Some structure and style

---
+++ src/App.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="app">
+    <main>
+      <div class="search-box">
+        <input type="text" class="search-bar" placeholder="Search" />
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      apikey: "44a0bb060c36bd8495c54fbb5a1101c6",
+    };
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: "Montserrat", sans-serif;
+}
+
+#app {
+  background-image: url("./assets/bg-cold.jpg");
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+}
+
+main {
+  min-height: 100vh;
+  padding: 2rem;
+  background-image: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0.25),
+    rgba(0, 0, 0, 0.75)
+  );
+}
+
+.search-box {
+  width: 100%;
+  margin-bottom: 3rem;
+}
+
+.search-box .search-bar {
+  display: block;
+  width: 100%;
+  padding: 1rem;
+  font-size: 1.5rem;
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+  color: #313131;
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 0px 16px 0px 16px;
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+  transition: 0.4s;
+}
+.search-box .search-bar:focus {
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 16px 0px 16px 0px;
+}
+</style>
